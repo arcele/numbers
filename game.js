26d8db8e1a80bffc245f46a57a62940bf9da517d
Ryan Kelley <arcele@gmail.com>
2023-08-03 21:08:03 -0700

    base game play

--- game.js
+++ game.js
@@ -1,3 +1,25 @@
 (function () {
   console.log("numbers.")
+  function setNewNumber() {
+    console.log("set new number.", $(".current-number").find(".number"));
+    $(".current-number").find(".number").text(Math.floor(Math.random() * 10 % 9))
+  }
+
+  $(document).ready( () => {
+    $(".blank-placed-number").on("click", (e) => {
+      let tar = $(e.target);
+      if(tar.hasClass("blank-placed-number")) {
+        $(e.target).removeClass("blank-placed-number").text($(".current-number").find(".number").text());
+        if($(".blank-placed-number").length > 0) {
+          setNewNumber();
+        } else {
+          $(".current-number").find(".number").text('-')
+        }
+      }
+    });
+
+    setNewNumber();
+
+  });
+
 })()
