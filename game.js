343bc5062115e2428b1560c8a17eecdd3d009022
Ryan Kelley <arcele@gmail.com>
2023-08-04 16:00:52 -0700

    allow starting a new game

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 (function () {
-  console.log("numbers.")
+
   function setNewNumber() {
-    console.log("set new number.", $(".current-number").find(".number"));
     $(".current-number").find(".number").text(Math.floor(Math.random() * 10 % 9))
   }
 
   $(document).ready( () => {
     $(".blank-placed-number").on("click", (e) => {
+      // Place a number
       let tar = $(e.target);
       if(tar.hasClass("blank-placed-number")) {
         $(e.target).removeClass("blank-placed-number").text($(".current-number").find(".number").text());
@@ -14,12 +14,16 @@
           setNewNumber();
         } else {
           $(".current-number").find(".number").text('-')
+          $(".game").addClass("full")
         }
       }
     });
-
+    $(".reset a").on("click", () => {
+      // Start New Game
+      $(".game").removeClass("full")
+      $(".placed-number").text("-").addClass("blank-placed-number")
+      setNewNumber();
+    })
     setNewNumber();
-
   });
-
-})()
+})();
