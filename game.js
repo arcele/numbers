0ece52904ee99f9610d71331d6b3a8b5368a1670
Ryan Kelley <arcele@gmail.com>
2023-08-14 18:10:33 -0700

    track placed numbers

--- game.js
+++ game.js
@@ -1,25 +1,34 @@
 (function () {
 
   this.numbers = [];
+  this.placedNumbers = [-1,-1,-1,-1,-1];
 
   function setNewNumber() {
     let newNumber = Math.floor(Math.random() * 10 % 9);
     $(".current-number").find(".number").text(newNumber);
     this.numbers.push(newNumber);
   }
+  function printNumberArray(arr) {
+    console.log("printing ", arr)
+    return parseInt(arr.toString().replaceAll(",",""));
+  }
 
   $(document).ready( () => {
     $(".blank-placed-number").on("click", (e) => {
       // Place a number
-      let tar = $(e.target);
+      let tar = $(e.target),
+          currentNumber = $(".current-number").find(".number").text();
       if(tar.hasClass("blank-placed-number")) {
-        $(e.target).removeClass("blank-placed-number").text($(".current-number").find(".number").text());
+        tar.removeClass("blank-placed-number").text(currentNumber);
+        this.placedNumbers[tar.attr("number-index")] = currentNumber;
         if($(".blank-placed-number").length > 0) {
           setNewNumber();
         } else {
           $(".current-number").find(".number").text('-')
           $(".game").addClass("full")
           console.log("all numbers:", this.numbers)
+          console.log("placed number:", printNumberArray(this.placedNumbers))
+          console.log("max :", printNumberArray(this.numbers.sort((a,b) => {return b-a})))
         }
       }
     });
@@ -28,6 +37,7 @@
       $(".game").removeClass("full")
       $(".placed-number").text("-").addClass("blank-placed-number")
       this.numbers = [];
+      this.placedNumbers = [-1,-1,-1,-1,-1];
       setNewNumber();
     })
     setNewNumber();
